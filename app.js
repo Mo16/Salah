24a56044561a7c05c01babaac7de30ecb31b4b9c
Mo16 <mosminecraft4@gmail.com>
2020-11-26 17:30:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-const express = require("express")
-const app = express()
-const scrape = require("./models/scraper")
-const oneHour = 60 * 60 * 1000
-
+const express = require("express");
+const app = express();
+const scrape = require("./models/scraper");
+const oneHour = 60 * 60 * 1000;
 
 app.use(express.static("public"));
-
 app.set("view engine", "ejs");
-
 app.get("/", function (req, res) {
   res.render("index.ejs");
 });
-
-app.listen(3000, function () {
-
-});
-
+app.listen(3000, function () {});
 
 function runScraper() {
-  scrape()
-  setTimeout(runScraper, oneHour)
+  scrape();
+  setTimeout(runScraper, oneHour);
 }
 
-runScraper()
+runScraper();
